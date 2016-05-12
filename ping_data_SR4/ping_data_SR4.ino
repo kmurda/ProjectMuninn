8bd78ac8ce45cb3a0926da070668b99330ea4cd8
Edmund Sannda <eksannda@gmail.com>
2016-05-12 10:08:11 -0500

    update the speed

--- ping_data_SR4/ping_data_SR4.ino
+++ ping_data_SR4/ping_data_SR4.ino
@@ -24,7 +24,7 @@
 #define echoPin4 9 // Yellow cable
 
 int const PRX = 45; // proximity
-int const MAX = 65; // max
+int const MAX = 85; // max
 
 
 long duration, distance, RightSensor, FrontSensor, LeftSensor, BackSensor;
@@ -247,7 +247,7 @@ void proc_main(int FrontSensor, int BackSensor, int RightSensor, int LeftSensor)
   
   }else if(front == 1 && back == 1 && right == 0 && left == 0){
 	//testing
-	Serial.print("stop");  
+	Serial.print("land");  
   
   }else if(front == 1 && back == 1 && right == 0 && left == 1){
 	//testing
